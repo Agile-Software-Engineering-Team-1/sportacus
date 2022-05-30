aebea28edf5b69a62e9338a185572a464c3d4a89
Eric Drejza <edrejza@udel.edu>
2022-05-30 12:38:35 -0400

    Created an initial layout for the homepage.
    Created components.
    
    Restructured our file structure.

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Header from "../Header/Header"
+import TeamsContainer from "../TeamsContainer/TeamsContainer";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <TeamsContainer />
+    </div>
+  );
+}
+
+export default App;
