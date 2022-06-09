e28ca77b4d19cdc89d0c05012ede6c628f973941
Jeffrey Lehman <JeffreyLehman94@gmail.com>
2022-06-08 22:23:48 -0400

    Gives a general idea of routing. Hooked the header up to actual pages.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,26 @@ import React from "react";
 import './App.css';
 import Header from "../Header/Header"
 import TeamsContainer from "../TeamsContainer/TeamsContainer";
+import Stats from "../Stats/Stats"
+import LoginPage from "../LoginPage/LoginPage"
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <TeamsContainer />
+      <Router>
+        <Routes>
+          <Route exact path="/LoginPage" element={<LoginPage/>}/>
+          <Route exact path="/Stats" element={<Stats/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,10 @@ import "./Header.css";
 function Header() {
   return (
     <nav className="topnav">
-      <a className='nav-link' href="#home">Home</a>
-      <a className='nav-link' href="#news">Login</a>
-      <a className='nav-link' href="#contact">Contact</a>
-      <a className='nav-link' href="#about">About</a>
+      <a className='nav-link' href="/Stats">Home</a>
+      <a className='nav-link' href="/LoginPage">Login</a>
+      <a className='nav-link' href="/Contact">Contact</a>
+      <a className='nav-link' href="/About">About</a>
     </nav>
   );
 }
