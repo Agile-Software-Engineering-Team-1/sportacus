213a0f2a5a4428be79757b05a2bda413634ea27c
Jeffrey Lehman <JeffreyLehman94@gmail.com>
2022-06-12 15:25:40 -0400

    Header dynamically changes login to logout if the user is already logged in

--- sporticus/urls.py
+++ sporticus/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('admin/', admin.site.urls),
-    path('api/', include('authentication.urls'))
+    path('api/', include('authentication.urls')),
 ]
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment }  from 'react';
 import "./Header.css";
+import { Link } from 'react-router-dom';
 
 function Header() {
+
+// The following code is used to tell if a user is logged in
+  const [isAuth, setIsAuth] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem('token') !== null) {
+      setIsAuth(true);
+    }
+  }, []);
+// End
+
+
   return (
     <nav className="topnav">
+      {isAuth === true ? (
+          <Fragment>
+            {' '}
       <a className='nav-link' href="/Stats">Home</a>
-      <a className='nav-link' href="/LoginPage">Login</a>
       <a className='nav-link' href="/Contact">Contact</a>
       <a className='nav-link' href="/About">About</a>
+      <a className='nav-link' href="/LoginPage">Logout</a>
+          </Fragment>
+            ) : (
+    <fragment>
+      <a className='nav-link' href="/Stats">Home</a>
+      <a className='nav-link' href="/Contact">Contact</a>
+      <a className='nav-link' href="/About">About</a>
+      <a className='nav-link' href="/LoginPage">Login</a>
+      </fragment>
+         )}
     </nav>
   );
 }
