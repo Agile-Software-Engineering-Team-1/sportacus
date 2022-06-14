5d41fa56d7a5cbaa661362d8b3e4ee95cc7a952f
BConti16 <58795371+BConti16@users.noreply.github.com>
2022-06-14 17:51:55 -0400

    Feature/function to get schedule data for nfl teams (#32)
    * add function to build nfl team schedules for a given year
    
    * update dictionary name in index.html
    
    * add routing to enable building only 1 team schedule at a time
    
    * add ending / to url to handle both / and no /

--- sporticus/urls.py
+++ sporticus/urls.py
@@ -21,6 +21,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', views.index, name="index"),
     path('admin/', admin.site.urls),
     path('api/', include('authentication.urls')),
+    path('nfl/<str:abbrv>/', views.renderNflTeamSchedule, name="team_schedule")
 ]
--- sporticus/views.py
+++ sporticus/views.py
@@ -1,15 +1,19 @@
 from django.http import HttpResponse
 from django.template import loader
 from sportsipy.nfl.teams import Teams
+from sportsipy.nfl.schedule import Schedule
 import json
 import os
+from datetime import date
 import argparse
 
+last_season = str(int(date.today().year) - 1)
 json_dir = "sporticus/json-data/"
 if(not os.path.exists(json_dir)):
     os.makedirs(json_dir)
 
-teams = Teams(year="2021")
+teams = Teams(year=last_season)
+team_abrvs = [team.abbreviation for team in teams]
 
 #Takes in the file (full path) and the list of json data
 #Writes the contents of the list to the .json file formatted properly for React to read
@@ -61,6 +65,67 @@ def buildTeamDict(teams, year) :
 
     return team_dict
 
+def buildNflSchedule(team_abrvs, year):
+    json_file_path = os.path.join(json_dir, "nfl-schedules.json")
+    schedule_dict = {}
+    schedule_list = []
+    x = 1
+    for team in team_abrvs:
+        schedule = Schedule(team, year)
+        game_dict = {}
+        game_list = []
+
+        y = 1
+        for game in schedule:
+            field_goal_att = game.field_goals_attempted
+            if(field_goal_att == 0):
+                field_goal_att = 1
+
+            game_dict[f"game{y}"] = {"date":f"{game.date}", "year":f"{year}", "opponent":f"{game.opponent_name}",
+            "scored":f"{game.points_scored}", "allowed":f"{game.points_allowed}",
+            "fld_goal_pct":f"{round((game.field_goals_made / field_goal_att) * 100, 2)}", "fourth_down_attempts":f"{game.fourth_down_attempts}",
+            "fourth_down_conv":f"{game.fourth_down_conversions}", "third_down_attempts":f"{game.third_down_attempts}",
+            "third_down_conv":f"{game.third_down_conversions}", "pass_tds":f"{game.pass_touchdowns}",
+            "pass_cmp_rate":f"{game.pass_completion_rate}", "pass_yards":f"{game.pass_yards}",
+            "ints":f"{game.interceptions}", "rush_yds":f"{game.rush_yards}", "rush_tds":f"{game.rush_touchdowns}",
+            "times_sacked":f"{game.times_sacked}"}
+
+            found = False
+            for dict in game_list:
+                if(dict["date"] == game_dict[f"game{y}"]["date"] and dict["opponent"] == game_dict[f"game{y}"]["opponent"]):
+                    found = True
+            if(not found):
+                game_list.append(game_dict[f"game{y}"])
+                
+            y += 1
+        
+        schedule_dict[f"{x}"] = {"team":f"{team}", "games":game_list}
+        x += 1
+    
+    for sched in schedule_dict.values():
+        schedule_list.append(json.dumps(sched, indent=4))
+    
+        #Remove old data each time this function is called
+    if(os.path.exists(json_file_path)):
+        os.remove(json_file_path)
+    
+    try:
+        writeJsonToFile(json_file_path, schedule_list)
+    except argparse.ArgumentTypeError as e:
+        print(e)
+
+    return schedule_dict
+
 def index(request):
     template = loader.get_template("index.html")
-    return HttpResponse(template.render({ "data" : buildTeamDict(teams, "2021")}, request))
+    return HttpResponse(template.render({ "team_data" : buildTeamDict(teams, last_season)}, request))
+
+def renderNflTeamSchedule(request, abbrv):
+    template = loader.get_template("index.html")
+    
+    team_abbrv = [abbrv.strip().upper()]
+    if(team_abbrv[0] not in team_abrvs):
+        print("Error: Team - " + team_abbrv[0] + " not found in nfl teams.")
+        return HttpResponse(template.render({}, request))
+        
+    return HttpResponse(template.render({ "team_data" : buildNflSchedule(team_abbrv, last_season)}, request))
