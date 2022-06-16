1bf3c72e9addf6b9a650e622e3616f7b512836f5
Brett Conti <baconti16@gmail.com>
2022-06-16 17:52:47 -0400

    add choice of year to routing and build schedules for past 3 years

--- sporticus/views.py
+++ sporticus/views.py
@@ -67,60 +67,69 @@ def buildTeamDict(teams, year) :
 
 def buildNflSchedule(team_abrvs, year):
     json_file_path = os.path.join(json_dir, "nfl-schedules.json")
-    schedule_dict = {}
-    schedule_list = []
-    x = 1
-    for team in team_abrvs:
-        schedule = Schedule(team, year)
-        game_dict = {}
-        game_list = []
+    year_dict = {}
+    year_list = []
 
-        y = 1
-        for game in schedule:
-            field_goal_att = game.field_goals_attempted
-            if(field_goal_att == 0):
-                field_goal_att = 1
+    for season in range((year - 2), (year + 1)):
+        schedule_dict = {}
+        schedule_list = []
+        x = 1
+        for team in team_abrvs:
+            schedule = Schedule(team, season)
+            game_dict = {}
+            game_list = []
 
-            game_dict[f"game{y}"] = {"date":f"{game.date}", "year":f"{year}", "opponent":f"{game.opponent_name}",
-            "scored":f"{game.points_scored}", "allowed":f"{game.points_allowed}",
-            "fld_goal_pct":f"{round((game.field_goals_made / field_goal_att) * 100, 2)}", "fourth_down_attempts":f"{game.fourth_down_attempts}",
-            "fourth_down_conv":f"{game.fourth_down_conversions}", "third_down_attempts":f"{game.third_down_attempts}",
-            "third_down_conv":f"{game.third_down_conversions}", "pass_tds":f"{game.pass_touchdowns}",
-            "pass_cmp_rate":f"{game.pass_completion_rate}", "pass_yards":f"{game.pass_yards}",
-            "ints":f"{game.interceptions}", "rush_yds":f"{game.rush_yards}", "rush_tds":f"{game.rush_touchdowns}",
-            "times_sacked":f"{game.times_sacked}"}
+            y = 1
+            for game in schedule:
+                field_goal_att = game.field_goals_attempted
+                if(field_goal_att == 0):
+                    field_goal_att = 1
 
-            found = False
-            for dict in game_list:
-                if(dict["date"] == game_dict[f"game{y}"]["date"] and dict["opponent"] == game_dict[f"game{y}"]["opponent"]):
-                    found = True
-            if(not found):
-                game_list.append(game_dict[f"game{y}"])
+                game_dict[f"game{y}"] = {"date":f"{game.date}", "year":f"{season}", "opponent":f"{game.opponent_name}",
+                "scored":f"{game.points_scored}", "allowed":f"{game.points_allowed}",
+                "fld_goal_pct":f"{round((game.field_goals_made / field_goal_att) * 100, 2)}", "fourth_down_attempts":f"{game.fourth_down_attempts}",
+                "fourth_down_conv":f"{game.fourth_down_conversions}", "third_down_attempts":f"{game.third_down_attempts}",
+                "third_down_conv":f"{game.third_down_conversions}", "pass_tds":f"{game.pass_touchdowns}",
+                "pass_cmp_rate":f"{game.pass_completion_rate}", "pass_yards":f"{game.pass_yards}",
+                "ints":f"{game.interceptions}", "rush_yds":f"{game.rush_yards}", "rush_tds":f"{game.rush_touchdowns}",
+                "times_sacked":f"{game.times_sacked}"}
+
+                found = False
+                for dict in game_list:
+                    if(dict["date"] == game_dict[f"game{y}"]["date"] and dict["opponent"] == game_dict[f"game{y}"]["opponent"]):
+                        found = True
+                if(not found):
+                    game_list.append(game_dict[f"game{y}"])
                 
-            y += 1
+                y += 1
         
-        schedule_dict[f"{x}"] = {"team":f"{team}", "games":game_list}
-        x += 1
+            schedule_dict[f"{x}"] = {"team":f"{team}", "games":game_list}
+            x += 1
     
-    for sched in schedule_dict.values():
-        schedule_list.append(json.dumps(sched, indent=4))
+        for sched in schedule_dict.values():
+            schedule_list.append(sched)
+
+        year_dict[f"{season}"] = {"year":f"{season}", "schedule":schedule_list}
+
+    for season in year_dict.values():
+        year_list.append(json.dumps(season, indent=4))
     
         #Remove old data each time this function is called
     if(os.path.exists(json_file_path)):
         os.remove(json_file_path)
     
     try:
-        writeJsonToFile(json_file_path, schedule_list)
+        writeJsonToFile(json_file_path, year_list)
     except argparse.ArgumentTypeError as e:
         print(e)
 
-    return schedule_dict
+    return year_dict
 
 def index(request):
     template = loader.get_template("index.html")
     return HttpResponse(template.render({ "team_data" : buildTeamDict(teams, last_season)}, request))
 
-def renderNflTeamSchedule(request, abbrv):
+def renderNflTeamSchedule(request, abbrv, year=last_season):
     template = loader.get_template("index.html")
     
     team_abbrv = [abbrv.strip().upper()]
@@ -128,4 +137,4 @@ def renderNflTeamSchedule(request, abbrv):
         print("Error: Team - " + team_abbrv[0] + " not found in nfl teams.")
         return HttpResponse(template.render({}, request))
         
-    return HttpResponse(template.render({ "team_data" : buildNflSchedule(team_abbrv, last_season)}, request))
+    return HttpResponse(template.render({ "team_data" : buildNflSchedule(team_abbrv, year)}, request))
