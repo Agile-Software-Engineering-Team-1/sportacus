70be7aff47d4566094f20320d2c74099849e61f0
Eric Drejza <edrejza@udel.edu>
2022-06-19 20:07:27 -0400

    Initial changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import './App.css';
 import Header from "../Header/Header"
-import TeamsContainer from "../TeamsContainer/TeamsContainer"
 import Stats from "../Stats/Stats"
 import LoginPage from "../LoginPage/LoginPage"
 import Login from '../../Views/auth/Login'
 import Signup from '../../Views/auth/Signup'
 import Logout from '../../Views/auth/Logout'
+import Profile from '../Profile/Profile'
 
 
 import {
@@ -27,6 +27,7 @@ function App() {
           <Route exact path="/Login" element={<Login/>}/>
           <Route exact path="/Signup" element={<Signup/>}/>
           <Route exact path="/Logout" element={<Logout/>}/>
+          <Route exact path="/Profile" element={<Profile/>}/>
         </Routes>
       </Router>
     </div>
--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './Stats.css';
-import Header from "../Header/Header"
 import TeamsContainer from "../TeamsContainer/TeamsContainer";
 {/* import pages for routing */}
 
