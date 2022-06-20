7714107c1d22a6c46805e541742040b98b9dc320
Eric Drejza <edrejza@udel.edu>
2022-06-20 10:40:07 -0400

    Added a colon to the favorite labels.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,14 +7,14 @@ function Profile() {
       <h1>Profile</h1>
       <label className='profile-item' htmlFor="username">Username: {/* GET username */}</label>
       <label className='profile-item' htmlFor='email'>Email: {/* GET email */}</label>
-      <label className='profile-item' htmlFor="NFL Favorite:">NFL Favorite
+      <label className='profile-item' htmlFor="NFL Favorite:">NFL Favorite:
         <select className='nfl-dropdown dropdown'>
           {/* Fill in options from what we can get from the API*/}
           <option value="">team name</option>
         </select>
       </label>
 
-      <label className='profile-item' htmlFor="NFL Favorite:">NCAA Favorite
+      <label className='profile-item' htmlFor="NFL Favorite:">NCAA Favorite:
         <select className='ncaa-dropdown dropdown'>
           {/* Fill in options from what we can get from the API*/}
           <option value="">team name</option>
