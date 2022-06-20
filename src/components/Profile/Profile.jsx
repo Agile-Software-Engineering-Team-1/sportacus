4c28bffddd0ed832c6b24bec0ef17212b9bb6492
Eric Drejza <edrejza@udel.edu>
2022-06-20 11:35:24 -0400

    Use already defined Dropdown components.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect, Fragment } from 'react';
+import StatDataNFL from '../../json-data/nfl-teams.json'
+import StatDataNCAAF from "../../json-data/ncaaf-teams.json";
+import DropdownTeam from '../Dropdown/DropdownTeam';
 import './Profile.css';
 
 function Profile() {
@@ -8,17 +11,11 @@ function Profile() {
       <label className='profile-item' htmlFor="username">Username: {/* GET username */}</label>
       <label className='profile-item' htmlFor='email'>Email: {/* GET email */}</label>
       <label className='profile-item' htmlFor="NFL Favorite:">NFL Favorite:
-        <select className='nfl-dropdown dropdown'>
-          {/* Fill in options from what we can get from the API*/}
-          <option value="">team name</option>
-        </select>
+        <DropdownTeam className='dropdown' statFile={StatDataNFL} />
       </label>
 
       <label className='profile-item' htmlFor="NFL Favorite:">NCAA Favorite:
-        <select className='ncaa-dropdown dropdown'>
-          {/* Fill in options from what we can get from the API*/}
-          <option value="">team name</option>
-        </select>
+        <DropdownTeam className='dropdown' statFile={StatDataNCAAF} />
       </label>
 
       <input className='profile-item save' type="submit" value="SAVE" />
