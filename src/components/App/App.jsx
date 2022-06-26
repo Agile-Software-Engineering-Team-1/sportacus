7ca636a8be0ea9eb830377f5ec3f71e9beb42992
TravisBiggs2 <105462517+TravisBiggs2@users.noreply.github.com>
2022-06-25 20:21:09 -0400

    Added StatsContainer as Home Page and Stats Page (link) (#57)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
 import Header from "../Header/Header"
-// import StatsContainer from "../StatsContainer/StatsContainer"
+import StatsContainer from "../StatsContainer/StatsContainer"
 import LoginPage from "../LoginPage/LoginPage"
 import Login from '../../Views/auth/Login'
 import Signup from '../../Views/auth/Signup'
@@ -22,8 +22,9 @@ function App() {
       <Header />
       <Router>
         <Routes>
+          <Route exact path="" element={<StatsContainer/>}/>
           <Route exact path="/LoginPage" element={<LoginPage/>}/>
-          {/* <Route exact path="/Stats" element={<StatsContainer/>}/> */}
+          <Route exact path="/Stats" element={<StatsContainer/>}/>
           <Route exact path="/Login" element={<Login/>}/>
           <Route exact path="/Signup" element={<Signup/>}/>
           <Route exact path="/Logout" element={<Logout/>}/>
