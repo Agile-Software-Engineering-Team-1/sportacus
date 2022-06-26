3e9f9636d572e4958324ba195d19abf773550335
Jeffrey Lehman <JeffreyLehman94@gmail.com>
2022-06-26 15:59:31 -0400

    Added favorite teams as text. Moved dropdown into comments since they do not work.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,12 +4,7 @@ import StatDataNCAAF from "../../json-data/ncaaf-teams.json";
 import DropdownTeam from '../Dropdown/DropdownTeam';
 import './Profile.css';
 
-function Profile() {
-  return (
-    <div className="profile-info">
-      <h1>Profile</h1>
-      <label className='profile-item' htmlFor="username">Username: {/* GET username */}</label>
-      <label className='profile-item' htmlFor='email'>Email: {/* GET email */}</label>
+/*
       <label className='profile-item' htmlFor="NFL Favorite:">NFL Favorite:
         <DropdownTeam className='dropdown' statFile={StatDataNFL} />
       </label>
@@ -17,7 +12,16 @@ function Profile() {
       <label className='profile-item' htmlFor="NFL Favorite:">NCAA Favorite:
         <DropdownTeam className='dropdown' statFile={StatDataNCAAF} />
       </label>
+*/
 
+function Profile() {
+  return (
+    <div className="profile-info">
+      <h1>Profile</h1>
+      <label className='profile-item' htmlFor="username">Username: {localStorage.username.substring(0,localStorage.username.length-1)}</label>
+      <label className='profile-item' htmlFor='email'>Email: {/* GET email */}</label>
+      <label className='profile-item' htmlFor='email'>Favorite NFl team: {localStorage.nfl}</label>
+      <label className='profile-item' htmlFor='email'>Favorite NCAAF team: {localStorage.col}</label>
       <input className='profile-item save' type="submit" value="SAVE" />
     </div>
   );
